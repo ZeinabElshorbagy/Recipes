e344e27fc511ef93164e5b0199b51320e46a68c7
ZeinabElshorbagy <zeinabelshorbagy@gmail.com>
2018-10-20 03:00:13 +0200

    recipes#index is done

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,31 @@
+class RecipesController < ApplicationController
+
+  def index
+    @recipes = Recipe.all
+  end
+
+  def new
+    #code
+  end
+
+  def create
+    #code
+  end
+
+  def show
+    #code
+  end
+
+  def edit
+    #code
+  end
+
+  def update
+    #code
+  end
+
+  def destroy
+    #code
+  end
+
+end
